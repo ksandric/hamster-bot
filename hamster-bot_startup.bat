2a4aa7752513d49b9afd3acf185cce9c4b0a9164
Shvaiba <alexandr.shvaiba@gmail.com>
2020-11-01 00:26:00 +0300

    Update hamster-bot_startup.bat

--- hamster-bot_startup.bat
+++ hamster-bot_startup.bat
@@ -1,12 +1,21 @@
 @echo off
 
-echo launch bot N1
-start "" "C:\bot\dreamcast_HiDeep\hamster-bot.exe"
+echo Starting bot #1
+cd "C:\bots\bot_1"
+start hamster-bot.exe
 
 echo wait 30 sec...
 ping 127.0.0.1 -n 30 > nul
 
-echo launch bot N2
-start "" "C:\bot\dreamcast_HiDeep_2\hamster-bot.exe"
+echo Starting bot #2
+cd "C:\bots\bot_2"
+start hamster-bot.exe
+
+echo wait 30 sec...
+ping 127.0.0.1 -n 30 > nul
+
+echo Starting bot #3
+cd "C:\bots\bot_3"
+start hamster-bot.exe
 
 exit
