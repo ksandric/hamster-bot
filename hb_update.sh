f5bd2f1eacbafcc9bf3386938103a2d412bf70b0
Sasha <alexandr.shvaiba@gmail.com>
2025-01-25 19:05:31 +0700

    2.0250115

---
+++ hb_update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# chmod +x hb_install.sh
+# ./hb_install.sh
+
+SERVICE_NAME="hamster-bot_1"
+SERVICE_FOLDER="hb"
+
+echo "Начало обновления бота /opt/$SERVICE_FOLDER"
+
+echo "Остановка сервиса"
+sudo systemctl stop $SERVICE_NAME.service
+
+cd /opt/$SERVICE_NAME
+echo "Скачивание новой версии бота"
+yes | wget -O /opt/$SERVICE_NAME/new_ver.zip https://github.com/ksandric/hamster-bot/blob/master/hb_linux-x64.zip?raw=true
+echo "Распаковка архива с заменой файлов"
+yes A | unzip -u new_ver.zip -x settings_program.json
+echo "Удаление файла архива"
+rm -rf new_ver.zip
+chmod +x ./hb_c
+echo "Запуск сервиса"
+sudo systemctl restart $SERVICE_NAME.service
+echo "Готово!"
+echo "Log:"
+sudo journalctl --follow -u $SERVICE_NAME.service
---
+++ mb_install.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# chmod +x hb_install.sh
+# ./hb_install.sh
+
+SERVICE_NAME="muslim-bot_1"
+SERVICE_FOLDER="mb"
+SERVICE_PORT="80"
+
+echo "Начало установки бота /opt/$SERVICE_FOLDER"
+
+echo "Установка: libfreetype6, libfontconfig1, fontconfig, fail2ban"
+yes | apt-get update
+yes | apt-get install -y libfreetype6
+yes | apt-get install -y libfontconfig1
+yes | apt-get install -y fontconfig
+yes | sudo timedatectl set-timezone UTC
+yes | apt-get install fail2ban
+
+mkdir -p /opt/$SERVICE_FOLDER
+cd /opt/$SERVICE_FOLDER
+
+echo "Скачать zip архив"
+yes | wget -O /opt/$SERVICE_FOLDER/new_ver.zip https://github.com/ksandric/hamster-bot/blob/master/mb_linux-x64.zip?raw=true
+yes | sudo apt install unzip
+echo "Распаковка архива"
+yes A | unzip -u new_ver.zip
+echo "Удаление файла архива"
+rm -rf new_ver.zip
+
+chmod +x ./hb_c
+# Создание systemd сервисного файла и его запуск
+# Определение пути к сервисному файлу и скрипту
+SERVICE_PATH="/usr/lib/systemd/system/$SERVICE_NAME.service"
+
+SERVICE_CONTENT="[Unit]
+Description=hamster-bot-$SERVICE_NAME
+After=multi-user.target
+[Service]
+Environment=\"DOTNET_BUNDLE_EXTRACT_BASE_DIR=%h/.net\"
+Type=simple
+ExecStart=/opt/$SERVICE_FOLDER/hb_c
+WorkingDirectory=/opt/$SERVICE_FOLDER
+RestartSec=61
+Restart=always
+[Install]
+WantedBy=multi-user.target"
+
+echo "Создание сервисного файла $SERVICE_PATH"
+echo "$SERVICE_CONTENT" | sudo tee $SERVICE_PATH
+
+echo "Перезагрузка systemd"
+sudo systemctl daemon-reload
+echo "Включение сервиса"
+sudo systemctl enable $SERVICE_NAME.service
+echo "Открытие порта $SERVICE_PORT"
+iptables -I INPUT -p tcp --dport $SERVICE_PORT -j ACCEPT
+ufw allow $SERVICE_PORT
+echo "Запуск сервиса"
+sudo systemctl start $SERVICE_NAME.service
+echo "Готово!"
+echo "Log:"
+sudo journalctl --follow -u $SERVICE_NAME.service
